d1d4f36ffbef86ec22d3ff135721f14b9a005a2b
Aryan Kapoor <69362333+kapooraryan@users.noreply.github.com>
2022-08-02 15:34:38 +0530

    Add files via upload

---
+++ credentials_spot-copy.py
@@ -0,0 +1,5 @@
+spot_client_id= "" #Generated Client ID
+spot_client_secret= "" #Generated Client Secret 
+spot_redirect_uri= 'http://localhost:3000/'  #This needs to be added into the http address (in spotify developer app settings)
+scope = 'user-read-currently-playing'
+username=""
---
+++ credentials_twt-copy.py
@@ -0,0 +1,5 @@
+twt_api_key = "" #Generated API Key
+twt_api_secret= "" #Generated Secret API Key 
+twt_acc= "" #Generated Access Token
+twt_acc_secret= "" #Generated Twitter Secret Access Token 
+
